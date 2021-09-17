c163b91a9dd41524457bed0280f5044c222c56b0
inevadim <kvadrocheyk@gmail.com>
2021-09-17 13:15:41 +0300

    create constructor, state and static defaultProps

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import './App.css';
+import React from 'react';
+import { WidthProvider, Responsive } from 'react-grid-layout';
+import _ from 'lodash';
 
-function App() {
-  return <div className="App"></div>;
+export default class AddRemoveLayout extends React.PureComponent {
+  static defaultProps = {
+    className: 'items',
+    cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
+    rowHeight: 100,
+    typeItem: 1,
+  };
+  constructor(props) {
+    super(props);
+    const parser = (name, val) => (val === 'Infinity' ? Infinity : val);
+    this.state = JSON.parse(localStorage.getItem('items'), parser) ?? {
+      // Чтение
+      items: [0, 1, 2, 3, 4].map(function (i, key, list) {
+        return {
+          i: i.toString(),
+          x: i * 2,
+          y: 0,
+          w: 2,
+          h: 2,
+          urlSite: null,
+          typeItem: 1,
+          add: i === list.length - 1,
+        };
+      }),
+      newCounter: 0,
+    };
+  }
+
+  render() {
+    return <div></div>;
+  }
 }
-
-export default App;
